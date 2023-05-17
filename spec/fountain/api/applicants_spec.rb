2ba1faf0841fc3811ae6d6f5eafe73a85e897ea4
Scott Harvey <scott@scottharvey.co>
2023-05-18 06:36:26 +0700

    Add wrapper for bulk advance multiple applicants

--- spec/fountain/api/applicants_spec.rb
+++ spec/fountain/api/applicants_spec.rb
@@ -367,6 +367,33 @@ describe Fountain::Api::Applicants do
     end
   end
 
+  describe '.advance_applicants' do
+    before do
+      # Stubs for /v2/applicants/advance REST API
+      stub_authed_request(:post, '/v2/applicants/advance?stage-id')
+        .to_return(status: 204)
+
+      stub_authed_request(:post, '/v2/applicants/advance?stage-id')
+        .with(
+          body: {
+            ids: ['01234567-0000-0000-0000-000000000000'],
+            skip_automated_actions: true,
+          }.to_json
+        )
+        .to_return(status: 204)
+    end
+
+    it 'advances the applicants to a specific stage (ignoring non-standard arguments)' do
+      result = described_class.advance_applicants(
+        ['01234567-0000-0000-0000-000000000001'],
+        'stage-id',
+        skip_automated_actions: true,
+        invalid_arg: 'should not be included'
+      )
+      expect(result).to be true
+    end
+  end
+
   describe '.get_interview_sessions' do
     let(:slot) do
       {
